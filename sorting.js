52cdc7058329feef8a2926b8abe7ab1b1846c1d0
sudhbhav <sudhbhav@SUDHBHAV-IN.oradev.oraclecorp.com>
2018-04-01 00:45:53 +0530

    indentation changes

--- sorting.js
+++ sorting.js
@@ -1,42 +1,42 @@
 //This function decides if sorting is to be done in ascending or descending order using the variable
 //asc_r_dsc and then calls the respective function below
-function sorting(asc_r_dsc,sortfiled,tabledata){
-	
-	if(asc_r_dsc === 'dsc'){
-		return descending(sortfiled,tabledata);
-	}else{
-		return ascending(sortfiled,tabledata);
-	}
+function sorting(asc_r_dsc, sortfiled, tabledata) {
+
+    if (asc_r_dsc === 'dsc') {
+        return descending(sortfiled, tabledata);
+    } else {
+        return ascending(sortfiled, tabledata);
+    }
 }
 
 
-function ascending(sortfiled,tabledata){
+function ascending(sortfiled, tabledata) {
 
-	for(i=0;i<tabledata.length;i++){
-		for(j=0;j<tabledata.length;j++){
-			var temp;
-			if(tabledata[i][sortfiled] < tabledata[j][sortfiled]){
-				temp = tabledata[i];
-				tabledata[i] = tabledata[j];
-				tabledata[j] = temp; 
-			}
-		}
-	}
-	return tabledata;
+    for (var i = 0; i < tabledata.length; i++) {
+        for (var j = 0; j < tabledata.length; j++) {
+            var temp;
+            if (tabledata[i][sortfiled] < tabledata[j][sortfiled]) {
+                temp = tabledata[i];
+                tabledata[i] = tabledata[j];
+                tabledata[j] = temp;
+            }
+        }
+    }
+    return tabledata;
 }
 
 
-function descending(sortfiled,tabledata){
+function descending(sortfiled, tabledata) {
 
-	for(i=0;i<tabledata.length;i++){
-		for(j=0;j<tabledata.length;j++){
-			var temp;
-			if(tabledata[i][sortfiled] > tabledata[j][sortfiled]){
-				 temp = tabledata[i];
-				 tabledata[i] = tabledata[j];
-				 tabledata[j] = temp; 
-			}
-		}
-	}
-	return tabledata;
+    for (var i = 0; i < tabledata.length; i++) {
+        for (var j = 0; j < tabledata.length; j++) {
+            var temp;
+            if (tabledata[i][sortfiled] > tabledata[j][sortfiled]) {
+                temp = tabledata[i];
+                tabledata[i] = tabledata[j];
+                tabledata[j] = temp;
+            }
+        }
+    }
+    return tabledata;
 }
